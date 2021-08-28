797bd58428b1fd15185dffc97a8ba1fcec27777d
nijiya <fathimathulnijiya@gmail.com>
2021-08-28 10:17:35 -0700

    commited all the balance files

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var MongoClient=require('mongodb').MongoClient
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});     
+router.post('/submit',function(req,res){
+console.log(req.body)
+MongoClient.connect("mongodb://localhost:27017", function(err,client){
+  if(err)
+   console.log("error")
+   else
+   console.log("database connected")
+   client.db('crossroads').collection('user').insertOne(req.body)
+})
+res.send("Got it")
+})
+module.exports = router;
